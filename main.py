195d31efe1c5633e67be5332fca23fb5d23df83b
Nitin Shrivastava <nshrivastava@expediagroup.com>
2021-05-08 16:06:58 +0530

    adding requirements

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def plot(data):
 
 if __name__ == '__main__':
     company_name = [ 'reliance', 'rain industries']
-    path = "<add_the_directory_path_here"
+    path = "<add_the_directory_path_here>"
     path='/Users/nshrivastava/Downloads'
     data = load_xlsx(path, company_name, 'profit & loss', 'sales')
     plot(data)
